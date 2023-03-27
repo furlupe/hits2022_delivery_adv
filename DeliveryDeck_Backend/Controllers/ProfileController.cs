f12ea8c6ad607f37d6423190564740a294b091cf
Ilia Volgin <228133713372282004@stud.tsu.ru>
2023-03-27 17:08:35 +0700

    Add project files.

---
+++ DeliveryDeck_Backend/Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryDeck_Backend.Controllers
+{
+    [ApiController]
+    [Route("profile")]
+    public class ProfileController : ControllerBase
+    {
+        [HttpGet]
+        public Task<IActionResult> GetProfile() 
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPatch]
+        public Task<IActionResult> UpdateProfile() 
+        { 
+            throw new NotImplementedException(); 
+        }
+    }
+}
